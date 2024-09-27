c2cf455322b17625885e57b3578c0a3bcb898306
arrrvldmr <arrrvldmr@yahoo.com>
2024-09-27 19:08:32 +0300

    menuupdate2

--- hooks/useGetMenuList.tsx
+++ hooks/useGetMenuList.tsx
@@ -5,17 +5,17 @@ const useGetMenuList = () => {
 
   return [
     {
-      iconSrc: '/assets/svg/menuList/fish.svg',
+      iconSrc: '/assets/svg/menuList/gustari.svg',
       name: t('menuList.main_dish'),
       slug: 'main_dish',
     },
     {
-      iconSrc: '/assets/svg/menuList/fish.svg',
+      iconSrc: '/assets/svg/menuList/lamb.svg',
       name: t('menuList.lamb'),
       slug: 'lamb',
     },
     {
-      iconSrc: '/assets/svg/menuList/fish.svg',
+      iconSrc: '/assets/svg/menuList/chicken.svg',
       name: t('menuList.chicken'),
       slug: 'chicken',
     },
@@ -25,27 +25,27 @@ const useGetMenuList = () => {
       slug: 'fish',
     },
     {
-      iconSrc: '/assets/svg/menuList/BBQ.svg',
+      iconSrc: '/assets/svg/menuList/garnish.svg',
       name: t('menuList.garnishes'),
       slug: 'garnishes',
     },
     {
-      iconSrc: '/assets/svg/menuList/BBQ.svg',
+      iconSrc: '/assets/svg/menuList/salad.svg',
       name: t('menuList.salads'),
       slug: 'salads',
     },
     {
-      iconSrc: '/assets/svg/menuList/BBQ.svg',
+      iconSrc: '/assets/svg/menuList/soup.svg',
       name: t('menuList.supe'),
       slug: 'supe',
     },
     {
-      iconSrc: '/assets/svg/menuList/BBQ.svg',
+      iconSrc: '/assets/svg/menuList/gozleme.svg',
       name: t('menuList.bread'),
       slug: 'bread',
     },
     {
-      iconSrc: '/assets/svg/menuList/BBQ.svg',
+      iconSrc: '/assets/svg/menuList/mamaliga.svg',
       name: t('menuList.mamaliga'),
       slug: 'mamaliga',
     },
